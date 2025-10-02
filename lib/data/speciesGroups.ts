84695301a05d72b623933c21a4bf6fccdd8f6508
Tom Sullivan <tsullivan79@gmail.com>
2025-10-02 15:35:42 -0500

    21B: add 'small-mammal' species group to resolver

--- lib/data/speciesGroups.ts
+++ lib/data/speciesGroups.ts
@@ -1,5 +1,5 @@
 // lib/data/speciesGroups.ts
-export type SpeciesGroup = "raptor" | "corvid";
+export type SpeciesGroup = "raptor" | "corvid" | "small-mammal";
 
 const RAPTOR: string[] = [
   // common raptors (lowercase, hyphenated slugs)
@@ -34,9 +34,21 @@ const CORVID: string[] = [
   "black-billed-magpie",
 ];
 
+const SMALL_MAMMAL: string[] = [
+  "eastern-gray-squirrel",
+  "fox-squirrel",
+  "red-squirrel",
+  "thirteen-lined-ground-squirrel",
+  "chipmunk",
+  "eastern-chipmunk",
+  "eastern-cottontail",
+  "cottontail",
+];
+
 const LOOKUP: Record<string, SpeciesGroup> = Object.fromEntries([
-  ...RAPTOR.map(s => [s, "raptor" as SpeciesGroup]),
-  ...CORVID.map(s => [s, "corvid" as SpeciesGroup]),
+  ...RAPTOR.map((s) => [s, "raptor" as SpeciesGroup]),
+  ...CORVID.map((s) => [s, "corvid" as SpeciesGroup]),
+  ...SMALL_MAMMAL.map((s) => [s, "small-mammal" as SpeciesGroup]),
 ]);
 
 export function speciesToGroup(slug?: string | null): SpeciesGroup | null {
